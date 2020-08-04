f35d176f9c1e8e4af6f2761f325dea49f28d4d29
Sammy <sammy.cullinan@gmail.com>
2020-08-04 11:48:17 -0700

    testing heroku

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+app.listen(80, () => {
+    console.log(`API server now on port 80!`);
 });
